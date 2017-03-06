38f85367f49c4250577e3e0b3aa3e58c6712c396
Kilian Chollet <kilian.chollet@atexo.com>
2017-03-06 14:31:10 +0100

    Sprint1

---
+++ Tableau2D.h
@@ -0,0 +1,34 @@
+#ifndef TABLEAU2D_H_INCLUDED
+#define TABLEAU2D_H_INCLUDED
+
+typedef int Item ;
+
+struct Tableau2D
+{
+    int lignes, colonnes;
+    Item **damier;
+};
+
+/* @brief initialiser, initialiser un damier de n lignes et p colonnes
+ * @param[in-out] t, tableau à 2 dimensions à initialiser(Tableau2D)
+ * @param[in] n, nombre de lignes
+ * @param[in] p, nombre de lignes
+*/
+void initialiser(Tableau2D& t, int n, int p);
+
+/* @brief detruire, détruit un Tableau2D
+ * @param[in-out] t, tableau à 2 dimensions à détruire (Tableau2D)
+*/
+void detruire(Tableau2D& t);
+
+/* @brief lire,
+ * @param[in-out] t, tableau à 2 dimensions à détruire (Tableau2D)
+*/
+void lire(Tableau2D& t);
+
+/* @brief afficher, affiche le tableau 2D
+ * @param[in] t, tableau 2D à afficher
+*/
+void afficher(const Tableau2D& t);
+
+#endif // TABLEAU2D_H_INCLUDED
